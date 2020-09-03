95530f3fd137b8693e404bc18af46aa3dd1ba17a
Alfred William Jacob <60570372+aLefred0@users.noreply.github.com>
2020-09-03 16:46:07 +0530

    Add files via upload

---
+++ AF event.py
@@ -0,0 +1,8 @@
+import random
+l=['Tarun','Hari Om ','Cyril ','Naveen Rathore','Sushant','Jathin','Akash Batchu','Meher','Kunal','Arjun','Abhimanyu','Shruti ','Aastha','Ayesha','Shashidhar ','Shishir','Kajal','Pranav']
+print(len(l))
+while l!=[] :
+    a=l[random.randint(0,len(l)-1)]
+    l.remove(a)
+    print(a)
+print(l)
