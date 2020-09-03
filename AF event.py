e8ca0a576e275e61dee2c6bd80c265d26a9c7fe9
Alfred William Jacob <60570372+aLefred0@users.noreply.github.com>
2020-09-03 16:48:02 +0530

    Update AF event.py

--- AF event.py
+++ AF event.py
@@ -1,9 +1,8 @@
 import random
-l=['Tarun','Hari Om ','Cyril ','Naveen Rathore','Sushant','Jathin','Akash Batchu','Meher','Kunal','Arjun','Abhimanyu','Shruti ','Aastha','Ayesha','Shashidhar ','Shishir','Kajal','Pranav']
+l=['Tarun','Hari Om ','Naveen Rathore','Sushant','Jathin','Akash Batchu','Meher','Kunal','Arjun','Abhimanyu','Shruti ','Aastha','Ayesha','Shashidhar ','Shishir','Kajal','Pranav']
 print(len(l))
 while l!=[] :
     a=l[random.randint(0,len(l)-1)]
     l.remove(a)
     print(a)
-print(l)
 input()
