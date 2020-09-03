25d24094a87769d4a25f4bbde4298bf14740d9c5
Alfred William Jacob <60570372+aLefred0@users.noreply.github.com>
2020-09-03 16:50:34 +0530

    Update AF event.py

--- AF event.py
+++ AF event.py
@@ -1,7 +1,12 @@
 import random
 l=['Tarun','Hari Om ','Naveen Rathore','Sushant','Jathin','Akash Batchu','Meher','Kunal','Arjun','Abhimanyu','Shruti ','Aastha','Ayesha','Shashidhar ','Shishir','Kajal','Pranav']
-print(len(l))
+l2=len(l)
+print("\nCalling People\n")
 while l!=[] :
+    if len(l)=l2-3:
+        print("\nMinor Event\n")
+    elif len(l)=l2-9:
+        print("\nMajor Event\n")
     a=l[random.randint(0,len(l)-1)]
     l.remove(a)
     print(a)
